9a92305eb6856de97317aadb73505f2e2886fa16
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-12 14:36:44 +0100

    HTML-CSS Milestone 1 completati

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+/*******************  MILESTONE 1 *********************/
+
+//Creare un layout base con una searchbar (una input e un button) in cui possiamo
+// scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// 1. Titolo
+// 2. Titolo Originale
+// 3. Lingua
+// 4. Voto
+
+/******************* MILESTONE 2 **********************/
+
 let app = new Vue({
   // options object
   el: "#app",
+  data: {
+    movies:
+  }
 });
